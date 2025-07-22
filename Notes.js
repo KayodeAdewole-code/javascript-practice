48206a3f51c4ffa89b17e5cf1fe1dc9149d2ce49
KayodeAdewole-code <Kayode.Adewole@yahoo.com>
2025-07-22 19:18:34 +0100

    javascriptpractice

---
+++ Notes.js
@@ -0,0 +1,13 @@
+// javascript notes
+
+// variables: 3 different variables are Var(oldest),Let(any variable that is going to chnage),Const(variables that you do not want to chnage)
+
+//identifiers: the aprt that comes after the variable and this will be what the value of the variable. (cant start with caps and cannot start with numbers)
+
+// data type: string=letters boolean= value is eaither true or false(cant be nothing else) null="it doesnt have a value but ir also does"
+
+let age=10;
+let numOfSlices=8;
+let name="Kayode";
+let favPlayer="My favourite player is Cole Palmer";
+let itRainToday=True;
