6212010b12c3b437a90ee8a9246bfd1f4a45048e
KayodeAdewole-code <kayode.adewole@yahoo.com>
2026-01-26 18:23:06 +0000

    new notes

---
+++ notes-recap.js
@@ -0,0 +1,27 @@
+let pizza=8
+let age=18
+let trainers=5
+const name="Kayode"
+let car="Ford"
+let singer="Drake"
+
+
+console.log(pizza)
+let lNumberOfPizza= pizza + 8
+let futureAge= age * 3
+let pastAge = age - 8
+let trainerSales= trainers*2 *5
+let opener = "i am"
+let sentence= opener + name 
+let age2 = "i am kayode and my age is" + age + "years old"
+let shoeSizes=[10,9,7,11,3]
+shoeSizes[4]= 14
+let favfoot=["palmer", "Bruno", "Olise", "Dembele"]
+let bestBaller= "the best footballer of 2k25 is" + favfoot[3]
+
+if(trainers>3){
+    console.log("you have more than 3 trainer")
+} else{
+    console.log("you have less than 3 trainers")
+}
+
